3b06ffd55f534bf0e0fc00bd7e543cbc50b29765
pubgali125.com@gmail.com <pubgali125.com@gmail.com>
2024-03-22 14:16:29 +0500

    this is a simple calculator

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter your First Number : ",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter yuor Second Number : ",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of operator to perform Operation('-')",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multipilcation", "Division"],
+    },
+]);
+// console.log(answer);
+// conditional statment
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multipilcation") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Pleacs Select Valid Number");
+}
